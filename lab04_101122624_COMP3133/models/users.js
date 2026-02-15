5111599a795ef59d4952585790b5f2db1e334604
DistinctDynamo <nguyensteven578@gmail.com>
2026-02-15 16:37:46 -0500

    fixed required validator

--- lab04_101122624_COMP3133/models/users.js
+++ lab04_101122624_COMP3133/models/users.js
@@ -3,73 +3,73 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
-        require: [true, "Name is required"],
+        required: [true, "Name is required"],
         trim: true
     },
     username:{
         type: String,
-        require: [true, "Username is required"],
+        required: [true, "Username is required"],
         trim: true,
         minlength: [4, "Username should be at least 4 characters"],
         maxlength: [100, "Username should be at most 100 characters"]
     },
     email:{
         type: String,
-        require: true,
+        required: [true, "Email is required"],
         unique: true
     },
     address:{
         street:{
             type: String,
-            require: true,
+            required: [true, "Street is required"],
             trim: true
         },
         suite:{
             type: String,
-            require: true
+            required: true
         },
         city:{
             type: String,
-            require: true,
+            required: true,
             match:[/^([a-zA-Z]*\s*){0,6}$/, "Only letters allowed"]
         },
         zipcode:{
-            require: true,
+            required: true,
             match: [/^\d{5}-\d{4}$/, "Invalid zipcode format"]
         },
         geo:{
             lat:{
                 type: Number,
-                require: true
+                required: true
             },
             lng:{
                 type: Number,
-                require: true
+                required: true
             }
         }
     },
     phone:{
-        require: true,
+        required: true,
         match: [/^\d-\d{3}-\d{3}-\d{4}$/, "Invalid phone format"]
     },
     website:{
-        require: true,
+        required: true,
         match: [/^http|https/, "Missing header"]
     },
     company:{
         name:{
             type: String,
-            require: true,
+            required: true,
             trim: true
         },
         catchphrase:{
             type: String,
-            require: true,
+            required: true,
             trim: true
         },
         bs:{
             type: String,
-            require: true,
+            required: true,
             trim: true
         }
     }
